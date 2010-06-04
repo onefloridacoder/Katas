fdd4f8c8d917c6ea3e987640a2f98effeaf99dd5
John Smith <onefloridacoder@live.com>
2010-06-04 19:33:31 -0400

    Adding initial solution to repository.

---
+++ Learn.Linq.Aggregate/Learn.Linq.Aggregate/AggregateKata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Learn.Linq.Aggregate
+{
+    public class AggregateKata
+    {
+        pubilc void must_group_and_display_correct_seed_value()
+        {
+
+        }
+    }
+}
